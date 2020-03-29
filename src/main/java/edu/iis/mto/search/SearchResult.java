febf5b256d73b55d52f84b91c8e3636629dfc222
debek.kacper <215702@edu.p.lodz.pl>
2020-03-29 20:24:30 +0200

    Initial refactor and .gitignore changes

--- src/main/java/edu/iis/mto/search/SearchResult.java
+++ src/main/java/edu/iis/mto/search/SearchResult.java
@@ -27,7 +27,8 @@ public class SearchResult {
         private boolean found;
         private int position;
 
-        private Builder() {}
+        private Builder() {
+        }
 
         public Builder withFound(boolean found) {
             this.found = found;
