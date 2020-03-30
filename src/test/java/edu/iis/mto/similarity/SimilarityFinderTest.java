eb8b33961110c670f021b19e47768238307a2e9d
debek.kacper <215702@edu.p.lodz.pl>
2020-03-30 11:25:33 +0200

    Added additional tests and refactored some of the code

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -8,9 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
 
-    private int[] emptySeq1 = {};
-    private int[] emptySeq2 = {};
-    private int[] filledSeq1 = {1, 2, 3, 4, 5};
+    private int[] emptySeq = {};
+    private int[] eightElemSeq = {1, 2, 3, 4, 5, 6, 7, 8};
+    private int[] twoElemSeq = {1, 2};
+    private int[] fourElemSeq = {1, 3, 5, 7};
+    private int[] sixElemSeq = {1, 2, 3, 4, 5, 6};
     private SimilarityFinder finder;
 
     @BeforeEach void setUp() {
@@ -18,10 +20,26 @@ class SimilarityFinderTest {
     }
 
     @Test void checkWhenBothSequencesEmpty() {
-        assertEquals(finder.calculateJackardSimilarity(emptySeq1, emptySeq2), 1.0d);
+        assertEquals(finder.calculateJackardSimilarity(emptySeq, emptySeq), 1.0d);
     }
 
     @Test void checkWhenOneSequenceIsEmptyAndOneIsFilled() {
-        assertEquals(finder.calculateJackardSimilarity(filledSeq1, emptySeq2), 0.0d);
+        assertEquals(finder.calculateJackardSimilarity(eightElemSeq, emptySeq), 0.0d);
+    }
+
+    @Test void checkWhenBothSequencesAreTheSame() {
+        assertEquals(finder.calculateJackardSimilarity(eightElemSeq, eightElemSeq), 1.0d);
+    }
+
+    @Test void checkWhenBothSequencesAreDifferent_expected_0_25() {
+        assertEquals(finder.calculateJackardSimilarity(eightElemSeq, twoElemSeq), 0.25d);
+    }
+
+    @Test void checkWhenBothSequencesAreDifferent_expected_0_5() {
+        assertEquals(finder.calculateJackardSimilarity(eightElemSeq, fourElemSeq), 0.5d);
+    }
+
+    @Test void checkWhenBothSequencesAreDifferent_expected_0_75() {
+        assertEquals(finder.calculateJackardSimilarity(eightElemSeq, sixElemSeq), 0.75d);
     }
 }
