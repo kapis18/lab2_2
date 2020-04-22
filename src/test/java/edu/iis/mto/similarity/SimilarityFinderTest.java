15130147e43011a9ebcd552a3a9c407b4facecf6
debek.kacper <215702@edu.p.lodz.pl>
2020-04-22 15:38:59 +0200

    Moved all tests to a single class

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,12 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
 import edu.iis.mto.search.SequenceSearcherMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +18,17 @@ class SimilarityFinderTest {
     private int[] fourElemSeq = {1, 3, 5, 7};
     private int[] sixElemSeq = {1, 2, 3, 4, 5, 6};
     private SimilarityFinder finder;
+    private SimilarityFinder behaviourFinder;
+    private SequenceSearcherMock mock;
 
     @BeforeEach void setUp() {
-        finder = new SimilarityFinder(new SequenceSearcherMock());
+        mock = new SequenceSearcherMock();
+        mock.clearCounter();
+        finder = new SimilarityFinder(testSearcher);
+        behaviourFinder = new SimilarityFinder(mock);
     }
 
+    //State tests
     @Test void checkWhenBothSequencesEmpty() {
         assertEquals(1.0d, finder.calculateJackardSimilarity(emptySeq, emptySeq));
     }
@@ -42,4 +52,35 @@ class SimilarityFinderTest {
     @Test void checkWhenBothSequencesAreDifferent_expected_0_75() {
         assertEquals(0.75d, finder.calculateJackardSimilarity(eightElemSeq, sixElemSeq));
     }
+
+    //Behaviour tests
+    @Test void testSeqLengthBothZero_ExpectZeroInvocations() {
+        behaviourFinder.calculateJackardSimilarity(emptySeq, emptySeq);
+        assertEquals(0, mock.getInvocationsCounter());
+    }
+
+    @Test void testSeqLengthEightAndZero_ExpectEightInvocations() {
+        behaviourFinder.calculateJackardSimilarity(eightElemSeq, emptySeq);
+        assertEquals(8, mock.getInvocationsCounter());
+    }
+
+    @Test void testSeqLengthTwoAndFour_ExpectTwoInvocations() {
+        behaviourFinder.calculateJackardSimilarity(twoElemSeq, fourElemSeq);
+        assertEquals(2, mock.getInvocationsCounter());
+    }
+
+    @Test void testSeqLengthFourAndSix_ExpectFourInvocations() {
+        behaviourFinder.calculateJackardSimilarity(fourElemSeq, sixElemSeq);
+        assertEquals(4, mock.getInvocationsCounter());
+    }
+
+    private SequenceSearcher testSearcher = (elem, seq) -> {
+        int index = IntStream.range(0, seq.length).filter(i -> seq[i] == elem).findFirst().orElse(-1);
+        if (index != -1) {
+            return SearchResult.builder().withFound(true).build();
+        } else {
+            return SearchResult.builder().withFound(false).build();
+        }
+    };
+
 }
