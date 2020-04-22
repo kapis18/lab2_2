14800af3ec520ca150831a005c95f623c2f6afd2
debek.kacper <215702@edu.p.lodz.pl>
2020-04-22 15:40:32 +0200

    Removed algorithm definition from mock

--- src/main/java/edu/iis/mto/search/SequenceSearcherMock.java
+++ src/main/java/edu/iis/mto/search/SequenceSearcherMock.java
@@ -1,31 +1,22 @@
 package edu.iis.mto.search;
 
-import java.util.stream.IntStream;
-
 public class SequenceSearcherMock implements SequenceSearcher {
 
     private static int invocationsCounter = 0;
 
-    public SequenceSearcherMock(){
+    public SequenceSearcherMock() {
     }
-    @Override public SearchResult search(int elem, int[] seq) {
-        SearchResult.Builder builder = SearchResult.builder();
-        int index = IntStream.range(0, seq.length).filter(i -> seq[i] == elem).findFirst().orElse(-1);
-        if (index != -1) {
-            builder.withFound(true);
-        } else {
-            builder.withFound(false);
-        }
-        builder.withPosition(index);
 
+    @Override public SearchResult search(int elem, int[] seq) {
         invocationsCounter += 1;
-        return builder.build();
+        return SearchResult.builder().build();
     }
 
     public int getInvocationsCounter() {
         return invocationsCounter;
     }
-    public void clearCounter(){
+
+    public void clearCounter() {
         invocationsCounter = 0;
     }
 }
