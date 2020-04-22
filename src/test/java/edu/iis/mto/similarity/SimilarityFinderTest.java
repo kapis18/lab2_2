e0f845842e902dbde54a2521c85b4be4565312fa
debek.kacper <215702@edu.p.lodz.pl>
2020-04-22 16:09:21 +0200

    Adjusted tests based on the expected result

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,12 +1,9 @@
 package edu.iis.mto.similarity;
 
 import edu.iis.mto.search.SearchResult;
-import edu.iis.mto.search.SequenceSearcher;
 import edu.iis.mto.search.SequenceSearcherMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,39 +14,64 @@ class SimilarityFinderTest {
     private int[] twoElemSeq = {1, 2};
     private int[] fourElemSeq = {1, 3, 5, 7};
     private int[] sixElemSeq = {1, 2, 3, 4, 5, 6};
-    private SimilarityFinder finder;
     private SimilarityFinder behaviourFinder;
     private SequenceSearcherMock mock;
 
     @BeforeEach void setUp() {
         mock = new SequenceSearcherMock();
         mock.clearCounter();
-        finder = new SimilarityFinder(testSearcher);
         behaviourFinder = new SimilarityFinder(mock);
     }
 
     //State tests
     @Test void checkWhenBothSequencesEmpty() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(true).build());
         assertEquals(1.0d, finder.calculateJackardSimilarity(emptySeq, emptySeq));
     }
 
     @Test void checkWhenOneSequenceIsEmptyAndOneIsFilled() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(false).build());
         assertEquals(0.0d, finder.calculateJackardSimilarity(eightElemSeq, emptySeq));
     }
 
     @Test void checkWhenBothSequencesAreTheSame() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> SearchResult.builder().withFound(true).build());
         assertEquals(1.0d, finder.calculateJackardSimilarity(eightElemSeq, eightElemSeq));
     }
 
     @Test void checkWhenBothSequencesAreDifferent_expected_0_25() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> {
+            for (int i : seq) {
+                if (i == elem) {
+                    return SearchResult.builder().withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        });
         assertEquals(0.25d, finder.calculateJackardSimilarity(eightElemSeq, twoElemSeq));
     }
 
     @Test void checkWhenBothSequencesAreDifferent_expected_0_5() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> {
+            for (int i : seq) {
+                if (i == elem) {
+                    return SearchResult.builder().withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        });
         assertEquals(0.5d, finder.calculateJackardSimilarity(eightElemSeq, fourElemSeq));
     }
 
     @Test void checkWhenBothSequencesAreDifferent_expected_0_75() {
+        SimilarityFinder finder = new SimilarityFinder((elem, seq) -> {
+            for (int i : seq) {
+                if (i == elem) {
+                    return SearchResult.builder().withFound(true).build();
+                }
+            }
+            return SearchResult.builder().withFound(false).build();
+        });
         assertEquals(0.75d, finder.calculateJackardSimilarity(eightElemSeq, sixElemSeq));
     }
 
@@ -73,14 +95,4 @@ class SimilarityFinderTest {
         behaviourFinder.calculateJackardSimilarity(fourElemSeq, sixElemSeq);
         assertEquals(4, mock.getInvocationsCounter());
     }
-
-    private SequenceSearcher testSearcher = (elem, seq) -> {
-        int index = IntStream.range(0, seq.length).filter(i -> seq[i] == elem).findFirst().orElse(-1);
-        if (index != -1) {
-            return SearchResult.builder().withFound(true).build();
-        } else {
-            return SearchResult.builder().withFound(false).build();
-        }
-    };
-
 }
